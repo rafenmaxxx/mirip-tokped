52b0bcc2be42b05cc46da903bf687562885a8944
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-17 20:19:44 +0700

    update route.php

--- php/src/app/route.php
+++ php/src/app/route.php
@@ -1,25 +1,17 @@
 <?php
 header('Content-Type: application/json');
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    http_response_code(405);
-    echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
-    exit;
-}
-
 $route = $_GET['route'] ?? '';
 
 switch ($route) {
-    case 'home':
-        echo json_encode([
-            'status' => 'success',
-            'data' => [
-                'title' => 'Menampilkan produk',
-                'content' => 'Ini konten dari backend PHP :D (teks ini)'
-            ]
-        ]);
+    case 'product':
+        require_once __DIR__ . '/controller/c_product.php';
+        break;
+    case 'image':
+        require_once __DIR__ . '/controller/c_image.php';
         break;
     default:
         http_response_code(404);
         echo json_encode(['status' => 'error', 'message' => 'Route tidak ditemukan']);
+        break;
 }
