76dda499f7007b054561ce779a5bdf5ba66f260e
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-17 20:19:59 +0700

    feat: create m_product.php

---
+++ php/src/app/model/m_product.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../db/db.php';
+
+class Product
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = Database::getInstance()->getConnection();
+    }
+
+    public function getAll()
+    {
+        /* 
+            NOTE : fetchall ada potensi masalah klo ternyata isi tabel products ini ada ratusan juta... 
+            soalnya si memori db ga kuat :D
+        */
+
+        $stmt = $this->conn->prepare("SELECT * FROM products");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function getById($id)
+    {
+        $stmt = $this->conn->prepare("SELECT * FROM products WHERE id=:id");
+        $res = $stmt->execute([':id' => $id]);
+        return $res;
+    }
+}
