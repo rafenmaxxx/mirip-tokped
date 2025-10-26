17445bf79526848fcb1dcd1ee6355ffe1a9f7ebf
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-26 12:37:15 +0700

    feat: create product_detail.js

---
+++ php/src/public/js/lib/product-detail/product_detail.js
@@ -0,0 +1,33 @@
+import { GET } from "../../api/api.js";
+import { ChangeInnerHtmlById } from "../../util/component_loader.js";
+
+function LoadDetail(data) {
+  const res = data.data;
+  ChangeInnerHtmlById("p-name", res.product_name);
+  ChangeInnerHtmlById("p-price", "Rp." + res.price);
+  ChangeInnerHtmlById("p-desc", res.description);
+  ChangeInnerHtmlById(
+    "p-stock",
+    "<p><strong>Stok:</strong>" + res.stock + " pcs</p>"
+  );
+  ChangeInnerHtmlById("s-desc", res.store_description);
+  ChangeInnerHtmlById("s-name", res.store_name);
+  const p_img_url = "api/image?file=" + res.main_image_path;
+  ChangeInnerHtmlById(
+    "p-img",
+    `<img src="${p_img_url}" alt="${res.product_name} Image" class="product-image">`
+  );
+  const s_img_url = "api/image?file=" + res.store_logo_path;
+  ChangeInnerHtmlById(
+    "s-img",
+    `<img src="${s_img_url}" alt="${res.store_name} Logo" class="seller-avatar">`
+  );
+}
+
+function IsErr(err) {}
+
+export function InitProductDetail() {
+  const params = new URLSearchParams(window.location.search);
+  const param_id = params.get("id");
+  GET("/api/product", { id: param_id }, LoadDetail, IsErr);
+}
