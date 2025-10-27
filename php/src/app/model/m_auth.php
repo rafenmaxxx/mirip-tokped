79116e4f1a2bf34f7ec8f1bdc881b991c8f63699
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-27 20:58:15 +0700

    feat: create m_auth.php

---
+++ php/src/app/model/m_auth.php
@@ -0,0 +1,76 @@
+<?php
+require_once __DIR__ . '/../db/db.php';
+
+class Auth
+{
+    private $conn;
+
+    public function __construct()
+    {
+        $this->conn = Database::getInstance()->getConnection();
+        session_start();
+    }
+
+    public function login($email, $password)
+    {
+        try {
+            $stmt = $this->conn->prepare("
+            SELECT user_id, email, password, role 
+            FROM users 
+            WHERE email = :email
+            LIMIT 1
+        ");
+            $stmt->execute(['email' => $email]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if (!$user) {
+                return ["status" => false, "message" => "Email tidak ditemukan"];
+            }
+
+            if ($password != $user['password']) {
+                return ["status" => false, "message" => "Password salah"];
+            }
+
+
+            $_SESSION['user'] = [
+                'id' => $user['id'],
+                'email' => $user['email'],
+                'role' => $user['role']
+            ];
+
+            return [
+                "status" => true,
+                "user" => [
+                    "id" => $user['id'],
+                    "email" => $user['email'],
+                    "role" => $user['role']
+                ]
+            ];
+        } catch (PDOException $e) {
+            return ["status" => false, "message" => $e->getMessage()];
+        }
+    }
+
+
+    public function logout()
+    {
+
+        session_unset();
+        session_destroy();
+
+        return [
+            "status" => true,
+            "message" => "Logout berhasil"
+        ];
+    }
+
+    public function check()
+    {
+
+        if (isset($_SESSION['user'])) {
+            return $_SESSION['user'];
+        } else {
+            return "";
+        }
+    }
+}
