6b851000d04f5151451871d3b02b616470c65608
max <13523031@std.stei.itb.ac.id>
2025-10-28 17:12:39 +0700

    feat: implement seller registration functionality in c_seller.php

---
+++ php/src/app/controller/c_seller.php
@@ -0,0 +1,40 @@
+<?php
+require_once __DIR__ . '/../model/m_seller.php';
+
+$model = new Seller();
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'POST':
+        header('Content-Type: text/html; charset=utf-8');
+        $email = $_POST['email'] ?? null;
+        $name = $_POST['name'] ?? null;
+        $address = $_POST['address'] ?? null;
+        $password = $_POST['password'] ?? null;
+
+        $role = $_POST['role'] ?? null;
+
+        $data = $model->createUser($name, $email, $password, $address, $role, 0);
+        if ($data) {
+
+            echo "<script>
+                alert('Berhasil mendaftarkan $email!');
+                window.location.href = '/login';
+            </script>";
+        } else {
+            $role = strtolower($role);
+            echo `<script>
+                alert('Gagal mendaftar!');
+                window.location.href = '/register/$role';
+            </script>`;
+        }
+
+        // kalo SELLER -> register TOKO
+
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+        break;
+}
