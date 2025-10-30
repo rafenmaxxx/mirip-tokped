be891237a4892f138ac7df2ba91e1f8b37b7ff85
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-30 22:54:15 +0700

    feat: create c_checkout.php

---
+++ php/src/app/controller/c_checkout.php
@@ -0,0 +1,48 @@
+<?php
+require_once __DIR__ . '/../model/m_cart.php';
+
+$model = new Cart();
+$method = $_SERVER['REQUEST_METHOD'];
+
+if (!isset($_SESSION['user'])) {
+    echo "<script>
+                alert('Login dulu Bos !');
+                window.location.href = '/login';
+            </script>";
+    exit;
+}
+
+switch ($method) {
+    case 'POST':
+        $buyer_id = $_SESSION['user']['id'] ?? null;
+        $address = $_POST['address'] ?? null;
+        if (!$address) {
+            http_response_code(400);
+            echo json_encode([
+                'status' => 'error',
+                'message' => 'Shipping address wajib diisi'
+            ]);
+            exit;
+        }
+
+        $result = $model->checkout($buyer_id, $address);
+
+        if ($result['status'] === 'success') {
+            echo json_encode([
+                'status' => 'success',
+                'message' => $result['message'],
+                'redirect' => $result['redirect']
+            ]);
+        } else {
+            http_response_code(400);
+            echo json_encode([
+                'status' => 'error',
+                'message' => $result['message']
+            ]);
+        }
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+        break;
+}
