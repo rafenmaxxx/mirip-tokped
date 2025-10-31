f0a14a2b9e58fca1caef43444511d2081dadc81f
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-10-31 10:26:09 +0700

    update seller_home.js

--- php/src/public/js/lib/seller/home/seller_home.js
+++ php/src/public/js/lib/seller/home/seller_home.js
@@ -10,6 +10,16 @@ function LoadSellerData() {
         const res = data.data;
         ChangeInnerHtmlById("store-name", res.store_name);
         ChangeInnerHtmlById("store-description", res.store_description);
+        ChangeInnerHtmlById("total-product", res.total_products);
+        ChangeInnerHtmlById("total-revenue", res.total_revenue);
+        ChangeInnerHtmlById("low-stock", res.low_stock_products);
+        ChangeInnerHtmlById("pending-order", res.pending_orders);
+        const imgUrl = `/api/image?file=${res.store_logo_path}`;
+        console.log(imgUrl);
+        ChangeInnerHtmlById(
+          "profile-avatar",
+          `<img src="${imgUrl}" alt="Gambar Profil Toko">`
+        );
       }
     },
     () => {}
