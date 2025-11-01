ee9c71858bec35f916927a265289d170d47d4d7e
max <13523031@std.stei.itb.ac.id>
2025-11-01 20:07:07 +0700

    feat: add category controller and model with routing

---
+++ php/src/app/controller/c_category.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../model/m_category.php';
+
+$model = new Category();
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+        $product_id = $_GET['product_id'] ?? null;
+        if ($product_id) {
+            $data = $model->getCategoriesForProduct($product_id);
+        } else {
+            $data = $model->getAll();
+        }
+        echo json_encode(['status' => 'success', 'data' => $data]);
+        break;
+
+    case 'POST':
+        
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+        break;
+}
