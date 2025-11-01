245228057a03439a55aa7c387f3909c7eb18a848
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-11-01 16:05:01 +0700

    feat: create add_product.js

---
+++ php/src/public/js/lib/seller/add-product/add_product.js
@@ -0,0 +1,23 @@
+import { router } from "../../../../app.js";
+import { showModalConfirmation } from "../../general/modal.js";
+
+export function InitAddProduct() {
+  document.getElementById("harga").addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+  });
+
+  document.getElementById("stok").addEventListener("input", (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, "");
+  });
+
+  const batalBtn = document.getElementById("batalBtn");
+  batalBtn.addEventListener("click", () => {
+    showModalConfirmation(
+      "Yakin batal? ini ga akan kesimpen lho",
+      () => {
+        router.navigateTo("/");
+      },
+      () => {}
+    );
+  });
+}
