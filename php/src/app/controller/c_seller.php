875e37043e6067332aaa2234583e845154290939
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-11-02 22:22:05 +0700

    update c_seller.php

--- php/src/app/controller/c_seller.php
+++ php/src/app/controller/c_seller.php
@@ -5,33 +5,6 @@ $model = new Seller();
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch ($method) {
-    case 'POST':
-        header('Content-Type: text/html; charset=utf-8');
-        $email = $_POST['email'] ?? null;
-        $name = $_POST['name'] ?? null;
-        $address = $_POST['address'] ?? null;
-        $password = $_POST['password'] ?? null;
-
-        $role = $_POST['role'] ?? null;
-
-        $data = $model->createUser($name, $email, $password, $address, $role, 0);
-        if ($data) {
-
-            echo "<script>
-                alert('Berhasil mendaftarkan $email!');
-                window.location.href = '/login';
-            </script>";
-        } else {
-            $role = strtolower($role);
-            echo `<script>
-                alert('Gagal mendaftar!');
-                window.location.href = '/register/$role';
-            </script>`;
-        }
-
-        // kalo SELLER -> register TOKO
-
-        break;
 
     default:
         http_response_code(405);
