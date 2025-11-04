b8ec10b773098f09af19b60e7c7a49385b40a3ac
max <13523031@std.stei.itb.ac.id>
2025-11-04 17:30:10 +0700

    feat: implement product seller controller and enhance navbar functionality

---
+++ php/src/app/controller/c_product_seller.php
@@ -0,0 +1,50 @@
+<?php
+require_once __DIR__ . '/../model/m_product.php';
+
+$model = new Product();
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+        $search = $_GET['search'] ?? null;
+        $id = $_GET['id'] ?? null;
+        $store_id = $_GET['store_id'] ?? null;
+        $filter = $_GET['filter'] ?? null;
+        $title = $_GET['title'] ?? null;
+
+        if ($search) {
+            // Jika ada parameter search
+            $data = $model->getByName($search);
+        } else if ($id) {
+            // ada id
+            $data = $model->getDetailById($id);
+        } else if ($title) {
+            // ada title -> return list of title buat autocomplete
+            $data = $model->getTitle($title);
+        } else if ($filter) {
+            // ada filter -> filter product
+            $filterData = json_decode($filter, true);
+            $categories = $filterData['categories'] ?? [];
+            $minPrice = $filterData['minPrice'] ?? null;
+            $maxPrice = $filterData['maxPrice'] ?? null;
+
+            $data = $model->getFilterProduct($categories, $minPrice, $maxPrice);
+        } else if ($store_id) {
+            // Jika ada parameter store_id
+            $data = $model->getByStoreId($store_id);
+        } else {
+            // Jika tidak ada parameter
+            $data = $model->getAll();
+        }
+
+        echo json_encode(['status' => 'success', 'data' => $data]);
+        break;
+
+    case 'POST':
+        
+        break;
+    default:
+        http_response_code(405);
+        echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
+        break;
+}
