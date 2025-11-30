41cdeb49e2ae319f7b96fea311c73df2b5ee7066
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-11-30 07:45:35 +0700

    feat: create App.jsx

---
+++ react/src/check/App.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function Check() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/product") // pastikan backend kamu benar-benar ada di path ini
+      .then((res) => {
+        if (!res.ok) throw new Error("HTTP Error: " + res.status);
+        return res.json();
+      })
+      .then((json) => {
+        setData(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <>Loading...</>;
+  if (error) return <>Error: {error}</>;
+
+  return (
+    <>
+      <h1>Halo dari check</h1>
+      <h2>Data Product dari php api:</h2>
+
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+}
+
+export default Check;
