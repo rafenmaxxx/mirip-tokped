6b8e1bf5e83bb72e22f3831d61a20124e55ffc32
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-12-07 23:20:27 +0700

    update app.js

--- node/src/app.js
+++ node/src/app.js
@@ -1,24 +1,15 @@
 import express from "express";
 import routes from "./routes/index.js";
+import r_webpush from "./routes/r_webpush.js";
 import { notFound, errorHandler } from "./middleware/ErrorHandler.js";
-import { UserService } from "./service/s_user.js";
 
 const app = express();
 
-// Middleware dasar
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Health check
-app.get("/health", (req, res) => {
-  res.json({ status: "OK", timestamp: Date.now() });
-});
-
-
-// Routes utama
 app.use(routes);
 
-// Error middleware
 app.use(notFound);
 app.use(errorHandler);
 
