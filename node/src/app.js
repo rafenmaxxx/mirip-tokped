d2917aeaedc623a9c11e01c7137217098aadc122
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-12-07 23:45:28 +0700

    update app.js

--- node/src/app.js
+++ node/src/app.js
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { raw } from "express";
 import routes from "./routes/index.js";
-import r_webpush from "./routes/r_webpush.js";
 import { notFound, errorHandler } from "./middleware/ErrorHandler.js";
 
 const app = express();
+
+app.use((req, res, next) => {
+  const rawCookies = req.headers.cookie; // "sessid=abc123; foo=bar"
+  console.log("cookie raw : ", rawCookies);
+  req.cookies = Object.fromEntries(
+    rawCookies?.split("; ").map((c) => c.split("=")) || []
+  );
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
