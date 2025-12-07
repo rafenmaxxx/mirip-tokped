cec2749921946c4e6a0f672eb60b18818edef4f9
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-12-07 23:45:34 +0700

    update r_user.js

--- node/src/routes/r_user.js
+++ node/src/routes/r_user.js
@@ -3,9 +3,10 @@ import { UserController } from "../controller/c_user.js";
 
 const router = Router();
 
+router.get("/me", UserController.getMe);
+router.get("/me/:sessid", UserController.getMe);
 router.get("/", UserController.getAll);
 router.get("/:id", UserController.getById);
-router.get("/me/:sessid", UserController.getMe);
 router.post("/", UserController.create);
 router.delete("/:id", UserController.remove);
 
