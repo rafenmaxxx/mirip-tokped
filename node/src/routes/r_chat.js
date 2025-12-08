3ea4c43379d40fd5dab487a24142b52d08ee3f4d
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-12-08 20:32:14 +0700

    update r_chat.js

--- node/src/routes/r_chat.js
+++ node/src/routes/r_chat.js
@@ -3,13 +3,10 @@ import { ChatController } from "../controller/c_chat.js";
 
 const router = Router();
 
-// ambil semua chat room user
 router.get("/rooms", ChatController.getChatRooms);
-
-// ambil semua pesan pada room tertentu
 router.get("/messages/:storeId/:buyerId", ChatController.getMessages);
-
-// kirim pesan
 router.post("/send", ChatController.sendMessage);
+router.post("/start", ChatController.startNewChat); // Route baru
+router.get("/stores", ChatController.getStoresForChat); // Route baru
 
 export default router;
