c337fdd90ccbaad241a5a40cf07bc70cad031f39
max <13523031@std.stei.itb.ac.id>
2025-12-09 15:19:35 +0700

    update node/src/routes/index.js

--- node/src/routes/index.js
+++ node/src/routes/index.js
@@ -3,6 +3,7 @@ import helloRoute from "./r_hello.js";
 import userRoutes from "./r_user.js";
 import webpushRoutes from "./r_webpush.js";
 import authRoutes from "./r_auth.js";
+import flagsRoutes from "./r_flags.js";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/", helloRoute);
 router.use("/user", userRoutes);
 router.use("/notif", webpushRoutes);
 router.use("/auth", authRoutes);
+router.use("/flags", flagsRoutes);
 
 export default router;
