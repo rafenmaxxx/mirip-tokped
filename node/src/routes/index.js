75819846ff8e98ac777d6c222b3b8780f5e960f0
jhotlann <19623263@std.stei.itb.ac.id>
2025-12-09 10:38:37 +0700

    feat: auction routes

--- node/src/routes/index.js
+++ node/src/routes/index.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import helloRoute from "./r_hello.js";
 import userRoutes from "./r_user.js";
+import auctionRoutes from "./r_auctions.js";
+import auctionbidsRoutes from "./r_auctionbids.js";
 import webpushRoutes from "./r_webpush.js";
 
 const router = Router();
 
 router.use("/", helloRoute);
 router.use("/user", userRoutes);
+router.use("/auctions", auctionRoutes);
+router.use("/auction-detail", auctionRoutes, userRoutes, auctionbidsRoutes);
+router.use("/auctionbids", auctionbidsRoutes);
 router.use("/notif", webpushRoutes);
 
-export default router;
+export default router;  
